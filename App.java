fbeacc79208f3da3e62e4c54879551d02be1053f
Zlatin <72519328+kikiom@users.noreply.github.com>
2021-01-15 12:39:03 +0200

    Add files via upload

--- App.java
+++ App.java
@@ -1,81 +1,86 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Scanner myScnaner = new Scanner(System.in);
-        int n;
-        int m;
-        int err = 0;
-        System.out.print("Enter n: ");
-        n = myScnaner.nextInt();
-        System.out.print("Enter m: ");
-        m = myScnaner.nextInt();
-        if (n < 0 || n % 2 != 0) {
-            err++;
-        }
-        if (m < 0 || m % 2 != 0) {
-            err++;
-        }   
-        int myObj[] [] = new int[n] [m];
-        int myTrueObj[] [] = new int[n] [m];
-        if (err == 0) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < myObj[i].length; j++) {
-                    myObj[i][j] = myScnaner.nextInt();
-                    if (myObj[i][j] < 0) {
-                        err++;
-                    }   
-                }    
-            } 
-        }    
-        if (err == 0) {
-            for (int i = 0; i < myObj.length; i++) {
-                for (int j = 0; j < myObj[i].length; j++) {
-                    if (j + 2 <= m) {    
-                      if (myObj[i][j] != myObj[i][j + 1] && myObj[i][j + 1] != myObj[i][j + 2] ) {
-                            err++;
-                        } 
-                    }   
-                }
-            }
-        }
-        if (err == 0) {
-            int br = 0;
+
+    public static int[] [] rightArray(int n, int m,int [][]myArray){  //this segment is for building the second layer
+        int[][] list = new int[n][m];       //function output array
+        int br = 0;
             for (int i = n-1; i >= 0; i-=2) {
-                for (int j = 0; j < myObj[i].length; j+=4) {
-                    if (myObj[i][j] == myObj[i][j+1]) {
-                        myTrueObj[i][j] = 2+br*4;
-                        myTrueObj[i-1][j] = 2+br*4;
-                        myTrueObj[i][j+1] = 3+br*4;
-                        myTrueObj[i][j+2] = 3+br*4;
-                        myTrueObj[i-1][j+1] = 1+br*4;
-                        myTrueObj[i-1][j+2] = 1+br*4;
-                        myTrueObj[i][j+3] = 4+br*4;
-                        myTrueObj[i-1][j+3] = 4+br*4;
+                for (int j = 0; j < myArray[i].length; j+=4) {
+                    if (myArray[i][j] == myArray[i][j+1]) {
+                        list[i][j] = 2+br*4;
+                        list[i-1][j] = 2+br*4;
+                        list[i][j+1] = 3+br*4;
+                        list[i][j+2] = 3+br*4;
+                        list[i-1][j+1] = 1+br*4;
+                        list[i-1][j+2] = 1+br*4;
+                        list[i][j+3] = 4+br*4;
+                        list[i-1][j+3] = 4+br*4;
                     }
                     else {
-                        myTrueObj[i][j] = 3+br*4;
-                        myTrueObj[i][j+1] = 3+br*4;
-                        myTrueObj[i-1][j+1] = 1+br*4;
-                        myTrueObj[i-1][j] = 1+br*4;
-                        myTrueObj[i-1][j+2] = 2+br*4;
-                        myTrueObj[i-1][j+3] = 2+br*4;
-                        myTrueObj[i][j+3] = 4+br*4;
-                        myTrueObj[i][j+2] = 4+br*4;
+                        list[i][j] = 3+br*4;
+                        list[i][j+1] = 3+br*4;
+                        list[i-1][j+1] = 1+br*4;
+                        list[i-1][j] = 1+br*4;
+                        list[i-1][j+2] = 2+br*4;
+                        list[i-1][j+3] = 2+br*4;
+                        list[i][j+3] = 4+br*4;
+                        list[i][j+2] = 4+br*4;
                     }
                     br++;
                 }
             }
+        return list;
+   }
+    
+    
+    public static void main(String[] args) throws Exception {
+        Scanner myScnaner = new Scanner(System.in);
+        int n;      //row of array
+        int m;      //coloms of array
+        boolean flag_err = false;        //flag for a wrong input or no way to be build 
+        n = myScnaner.nextInt();    
+        m = myScnaner.nextInt();    
+        if (n < 0 || n % 2 != 0) {
+            flag_err = true;
         }
-        if (err > 0) {
+        if (m < 0 || m % 2 != 0) {
+            flag_err = true;
+        }   
+        int myArray[] [] = new int[n] [m];        //input array
+        int myTrueArray[] [] = new int [n][m];    //output array
+        if (flag_err == false) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < myArray[i].length; j++) {
+                    myArray[i][j] = myScnaner.nextInt();
+                    if (myArray[i][j] < 0) {
+                        flag_err = true;
+                    }   
+                }    
+            } 
+        }    
+        if (flag_err == false) {             //check for impossible solution
+            for (int i = 0; i < myArray.length; i++) {
+                for (int j = 0; j < myArray[i].length; j+=4) {
+                      
+                      if (j + 2 <= myArray[i].length && myArray[i][j] != myArray[i][j + 1] && myArray[i][j + 1] != myArray[i][j + 2] ) {
+                            flag_err = true;
+                        } 
+                       
+                }
+            }
+        }
+        if (flag_err == false) {                                     
+            myTrueArray = rightArray(n, m, myArray);
+        }
+        if (flag_err == true) {
             System.out.println("-1");
         }
-        if (err == 0) {
+        if (flag_err == false) {     //output for the solution
             
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < myTrueObj[i].length; j++) {
-                    System.out.print(myTrueObj[i][j] + " ");
+                for (int j = 0; j < myTrueArray[i].length; j++) {
+                    System.out.print(myTrueArray[i][j] + " ");
                 }
                 System.out.println();
             }
